e6a2e9fc51475725cc8c4de9f462fcbe415bdd88
MikiMirai <ased223@abv.bg>
2022-08-07 21:25:38 +0300

    Moved replacer class outside of main program file.

---
+++ SaiThemeColorChanger/ReplacerHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace SaiDarkTheme
+{
+    public class ReplacerHelper
+    {
+        public ReplacerHelper(string search, string replace)
+        {
+            Search = search;
+            Replace = replace;
+        }
+
+        public string Search { get; }
+        public string Replace { get; }
+    }
+}
